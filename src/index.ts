920d004772f115a120094259f53ed7065e14ba11
Tomasz Dysinski <td@horizongames.net>
2019-08-02 21:19:01 -0700

    seperating physics from lighting test. stubbing out easy way to run other tests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { Clock, Color, Vector3 } from 'three'
 
 import { simpleTweener } from './animation/tweeners'
+import { BaseTestScene } from './helpers/scenes/BaseTestScene'
 import TestLightingScene from './helpers/scenes/TestLighting'
+import TestPhysicsScene from './helpers/scenes/TestPhysics'
 import renderer from './renderer'
 import { timeUniform } from './uniforms'
 import { cameraShaker } from './utils/cameraShaker'
+import { getUrlParam } from './utils/location'
 import { nextFrameUpdate } from './utils/onNextFrame'
 import { taskTimer } from './utils/taskTimer'
 import UpdateManager from './utils/UpdateManager'
@@ -16,7 +19,17 @@ renderer.setClearColor(new Color(0x344556), 1.0)
 cameraShaker.camera.position.set(0, 0.5, 0.5)
 cameraShaker.camera.lookAt(new Vector3())
 
-const test = new TestLightingScene()
+const testClasses: { [K: string]: new () => BaseTestScene } = {
+  lighting: TestLightingScene,
+  physics: TestPhysicsScene
+}
+
+let TestClass: new () => BaseTestScene = TestLightingScene
+const testParam = getUrlParam('test') || 'lighting'
+if (testClasses.hasOwnProperty(testParam)) {
+  TestClass = testClasses[testParam]
+}
+const test: BaseTestScene = new TestClass()
 
 const loop = () => {
   const dt = Math.min(clock.getDelta(), 0.1) * simpleTweener.speed
