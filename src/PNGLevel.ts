ff2d4f9fe464da001b09a80b2ac13c461951a546
Tomasz Dysinski <td@horizongames.net>
2019-11-12 10:17:03 -0500

    cleanup of PNGLevel algorithm

--- src/PNGLevel.ts
+++ src/PNGLevel.ts
@@ -86,67 +86,41 @@ export default class PNGLevel {
       let accumilator = 0
       const colour = new Color()
       const nextColour = new Color()
-      for (let i = 0; i < data.length; i++) {
-        function log(msg: string) {
-          const x = i % width
-          const y = Math.floor(i / width)
-          console.log(`build? x(${x})y(${y}), ${msg}`)
-        }
-
-        function logc() {
-          const x = i % width
-          const y = Math.floor(i / width)
-          console.log(`position: x(${x})y(${y}), colour= ${colour.r} ${colour.g} ${colour.b},
-            cHex = ${cHex}, is opaque? = ${opaque}`)
-        }
-
-        function lognc() {
-          console.log(`nextColour= ${nextColour.r} ${nextColour.g} ${nextColour.b},
-            ncHex = ${ncHex}, is nextOpaque? = ${nextOpaque}`)
-        }
-
-        colour.r = data[i * 4 + 0] / 255
-        colour.g = data[i * 4 + 1] / 255
-        colour.b = data[i * 4 + 2] / 255
-        const cHex = colour.getHex()
+      function extractColour(c: Color, i: number) {
+        c.r = data[i * 4 + 0] / 255
+        c.g = data[i * 4 + 1] / 255
+        c.b = data[i * 4 + 2] / 255
+      }
+      const totalPixels = width * height
+      for (let i = 0; i < totalPixels; i++) {
+        extractColour(colour, i)
         const opaque = data[i * 4 + 3] > 128
-        //logc()
 
         const j = i + 1
-        nextColour.r = data[j * 4 + 0] / 255
-        nextColour.g = data[j * 4 + 1] / 255
-        nextColour.b = data[j * 4 + 2] / 255
-        const ncHex = nextColour.getHex()
+        extractColour(nextColour, j)
         const nextOpaque = data[j * 4 + 3] > 128
-        //lognc()
 
         let build = false
 
         if (opaque) {
           accumilator++
-          if (cHex !== ncHex) {
+          if (!colour.equals(nextColour)) {
             build = true
-            //log('next colour changes')
           }
           if (opaque !== nextOpaque) {
             build = true
-            //log('next opaque changes')
           }
         } else {
           build = true
-          //log('not opaque')
         }
-        if ((i + 1) % width === 0) {
+        if (j % width === 0) {
           build = true
-          //log('wrap-around')
         }
         if (build && accumilator > 0) {
           const iCol = i % width
           const iRow = Math.floor(i / width)
-          blockRecipes.register(iCol, iRow, accumilator, 1, cHex)
-          //log(`Accume= ${accumilator}, Colour= ${cHex}`)
+          blockRecipes.register(iCol, iRow, accumilator, 1, colour.getHex())
           accumilator = 0
-          //log('YES, BUILD EET!')
         }
       }
       blockRecipes.process(blockProcessor)
